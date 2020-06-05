f157887ac636a562f658464e771eddba6d537094
kartik1502 <64131481+kartik1502@users.noreply.github.com>
2020-06-05 16:17:12 +0530

    Add files via upload

---
+++ Smallest and the largest element in array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],i,n,large,small;
+    printf("Enter number of elements:");
+    scanf("%d", &n);
+    printf("Input the array element:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    large=small=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>large)
+        {
+            large=a[i];
+        }
+        if(a[i]<small)
+        {
+            small=a[i];
+        }
+        printf("The smallest element is %d\n", small);
+        printf("The largest element is %d\n", large);
+    }
+}
